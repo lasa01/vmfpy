209cf13171a0df1daaa230f62f43cb60df4149cd
Lassi Säike <lassisaike11@gmail.com>
2020-02-15 17:36:27 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="vmfpy",
-    version="0.2.1",
+    version="0.2.2",
     author="Lassi Säike",
     description="A Valve Map Format (VMF) parser",
     long_description=long_description,
